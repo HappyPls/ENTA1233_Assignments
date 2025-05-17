a98407e108f92e5153b4a0e624ec7b752832c3c1
HappyPls <bryangoh2000@gmail.com>
2025-05-16 17:30:25 -0700

    Update GunScript
    added some pseudocodes

--- ThirdPersonShooter/Assets/Scripts/GunScript.cs
+++ ThirdPersonShooter/Assets/Scripts/GunScript.cs
@@ -8,7 +8,9 @@ public class GunScript : MonoBehaviour
     protected int maxAmmo = 30;
     protected int damage = 10;
     protected float fireRate = 1.0f;
-    protected float reloadTime 5.0f;
+    protected float reloadTime = 5.0f;
+
+    //can create a reference to get animation, vfx and audio.
 
     protected int currentAmmo;
     protected bool isReloading = false;
@@ -16,5 +18,22 @@ public class GunScript : MonoBehaviour
     protected virtual void Start()
     {
         currentAmmo = maxAmmo;
+    }
+
+    protected virtual void Update()
+    {
+        //check if the gun is reloading
+        //if reload input key is pressed, start the reload animation and pause any other firing actions.
+        //change currentAmmo to maxAmmo on animation end
+        //Debug.Log ("Gun Reloaded!");
+        // if firing input is pressed, stop reload animation and keep currentAmmo instead of changing it to maxAmmo
+        return;
+    }
+
+    protected virtual void Shoot()
+    {
+        //check if currentAmmo = 0. If ammo is 0, start reload animation and disable shooting
+        //shooting script here
+        return;
     }
 }
